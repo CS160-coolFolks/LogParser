79b79f9cbfa642bcd92aaa89433db3e3c06dbc56
Ai Nakamura <ainakamura513@gmail.com>
2017-10-16 22:42:32 -0700

    added new file LogStringParser.py that reads lines instead of a file.

---
+++ LogStringParser.py
@@ -0,0 +1,71 @@
+'''
+this version takes in a big long string of the log file rather than a file object
+
+
+'''
+
+import re
+
+
+class LogStringReader:
+
+    def __init__(self, log, error_keys, known_errors, known_non_errors):
+        self.log_lines = log.split("\n")
+        self.non_errors = known_non_errors
+        self.error_keys = error_keys
+
+        # errors dictionary
+        self.errors = {}
+        for e in known_errors:
+            self.errors[e[0]] = []
+
+    '''
+    Search for potential new errors.
+    Skips the known_errors since we're only looking for potential new ones
+    :returns: maybe_new_error -- dictionary of the lines that contained the error_key(s), value is list of the triggered keys
+        ex: "this is the string containing 'fail' and 'error'" : ['fail', 'error']
+    '''
+    def maybe_new_errors(self):
+        maybe_new_error = {}
+        for line in self.log_lines:
+            # check if line contains any of the error keywords
+            if any (key in line for key in self.error_keys):
+                # don't bother if the line contains something from the known non_errors
+                if not any (non_error in line for non_error in self.non_errors):
+                    maybe_new_error[line] = []
+                    for k in self.error_keys:
+                        if str(k.upper()) in line.upper():
+                            maybe_new_error[line].append(k)
+
+        return maybe_new_error
+
+    '''
+    Appends a new error to the self.errors list
+    :parameter: new_errors -- a list of new errors given by the user 
+    '''
+    def add_new_error(self, new_errors):
+        for new in new_errors:
+            self.errors[new] = []
+
+    '''
+    Searches for the known errors, that is, the errors recorded in self.errors
+    :returns: self.error -- dictionary of Name of Error (key) and the lines that were caught on it (values)
+            the values themselves have a tuple of date, time, and original line
+    '''
+    def find_errors(self):
+        print("number of lines: " + str(len(self.log_lines)))
+        for f in self.log_lines:
+            for e in self.errors:
+                error_found = re.search(str(e), f, re.M | re.I)
+                # figure out regex expressions for finding varied length characters, ignoring spaces, etc
+                # re.search(‘principal=([^ ]*)‘, principal_line)[1]
+                # 'within string we're matching=(this is the capture we want[here are rules for one character ('^' means not the things after it)]* repeat as many times as it can)
+                if error_found:
+                    date = f[:6]
+                    time = f[7:15]
+                    principal_index = f.find('AuditEvent')
+                    principal_user = None
+                    # if principal_index > 0:
+                    #     principal_user = f[principal_index:].split()[6].split('=')[1].strip(',')
+                    self.errors[e].append((date, time, f))
+        return self.errors
---
+++ StringParserTester.py
@@ -0,0 +1,51 @@
+
+
+
+from LogStringParser import LogStringReader
+
+# ---------- needed input variables to run LogStringReader class -------------- #
+# the one long string of the log
+str_log = open('syslog.3', 'r').read()
+# the error keys to look out for possible new errors
+# having trouble filtering out errors of numbered values, so they're currently commented out
+error_keys = 'exception', 'warn', 'error', 'fail', 'unauthorized', 'timeout', 'refused', 'NoSuchPageException' #, '404', '401', '500'
+# the known words for confirmed errors.
+# is having this and the keys redundant?
+# I think i won't need the tuple values anymore since the server side will be holding the details of the errors themselves
+known_errors = [
+    ('HttpClientErrorException', 401, ''),
+    ('AccessDeniedException', 403, 'AUTHORIZATION_FAILURE')
+]
+# is there some way we can write the code so that the keys that are triggered but have a '0' in front won't be counted as an error?
+# something like 'from the discovered trigger, find the nearest ',' on either side and it theres a ' 0 ' (with spaces on both sides of the 0) then ignore this triggered error'
+# when we ask user to tell us why a 'maybe' line is not an error, we should probably ask them to highlight the exact part of a phrase that makes it a non_error
+known_non_errors = [
+    '0 connect failure',
+    '0 transport failure',
+    '0 closed abnormally'
+]
+
+# ----------------------------------------------------------------------------- #
+
+print ("Error log string received")
+lsp = LogStringReader(str_log, error_keys, known_errors, known_non_errors)
+
+# testing maybe_errors
+# maybes = lsp.maybe_new_errors()
+# for key in maybes:
+#     print (key)
+#     print (maybes[key])
+
+# testing add_errors --- won't be used
+# print (lsp.errors)
+# new_errors = ['timeout']
+# lsp.add_new_error(new_errors)
+# print (lsp.errors)
+
+# testing find_errors
+# log_errors = lsp.find_errors()
+# for key in log_errors:
+#     print (key)
+#     for value in log_errors[key]:
+#         print (value)
+
